d98ac13a85ab7aa7e438f3d7716dde1771f7755b
LeiYangGH <675686066@qq.com>
2017-08-02 19:55:28 -0700

    cd to project and run

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-dotnet restore ~/g/testMVC/WebApplication1/WebApplication1.sln
-dotnet build ~/g/testMVC/WebApplication1/WebApplication1.sln
-dotnet publish ~/g/testMVC/WebApplication1/WebApplication1/WebApplication1.csproj -o /home/leiyang/p
-dotnet /home/leiyang/p/WebApplication1.dll 
+#$dotnet restore ~/g/testMVC/WebApplication1/WebApplication1.sln
+#dotnet build ~/g/testMVC/WebApplication1/WebApplication1.sln
+#dotnet publish ~/g/testMVC/WebApplication1/WebApplication1/WebApplication1.csproj -o /home/ly/p
+#cd /home/ly/p
+#dotnet /home/ly/p/WebApplication1.dll 
+cd WebApplication1/WebApplication1
+#dotnet    run --project WebApplication1/WebApplication1/WebApplication1.csproj
+dotnet    run 
